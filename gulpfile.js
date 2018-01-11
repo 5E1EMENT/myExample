7a646e8cb8321a000e9503b22ddaafdd7ca2d310
Daniil Nikitas <daniil_nikitas@mail.ru>
2018-01-11 11:53:40 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+var gulp = require('gulp'),
+    sass = require("gulp-sass"),
+	gp   = require('gulp-load-plugins')(),
+	browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "../build/"
+        }
+    });
+    
+});
+
+
+gulp.task('sass', function () {
+	return gulp.src('scss/main.scss')
+	.pipe(gp.sourcemaps.init())
+	.pipe(gp.sass().on('error', sass.logError))
+	.pipe(gp.autoprefixer({
+            browsers: ['last 5 versions']
+        }))
+	.on("error", gp.notify.onError({
+        message: "Error: <%= error.message %>",
+        title: "Stile"
+      }))
+	.pipe(gp.csso())
+	.pipe(gp.sourcemaps.write())
+	.pipe(gulp.dest('css/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('scss/**/*.scss',gulp.series('sass'));
+});
+gulp.task('default',gulp.series(
+	gulp.parallel('sass'),
+	gulp.parallel('watch','serve')
+	
+
+	));
